9fd8395befa98161283987de12ee2e0e3241a474
D00Movenok <egor.gennadjev@yandex.ru>
2025-01-30 17:24:07 +0300

    add: cloudru goad-mini lab

---
+++ ad/GOAD-Mini/providers/cloudru/windows.tf
@@ -0,0 +1,11 @@
+# s7n.large.2 : 2 CPU / 4GB
+# s7n.large.4 : 2CPU / 8GB
+# s7n.xlarge.2 : 4 cpu / 8 GB
+# s7n.xlarge.4 : 4 cpu / 16 GB
+"dc01" = {
+  name               = "dc01"
+  os_image           = "Windows Server 2019 Datacenter 64bit English"
+  private_ip_address = "{{ip_range}}.10"
+  password           = "8dCT-DJjgScp"
+  size               = "s7n.large.2"
+}
